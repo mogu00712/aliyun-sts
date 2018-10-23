a039579b8ff74d88cf905755ca968c033263bed4
Leo Liu <liulong@shinetechchina.com>
2018-10-23 15:05:12 +0800

    first commit

---
+++ config/aliyun_sts.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    'endpoint'    => env('ALIYUN_ENDPOINT', 'cn-beijing'),
+    'region'      => env('ALIYUN_REGION', 'cn-beijing'),
+    'domain'      => env('ALIYUN_DOMAIN', 'sts.cn-beijing.aliyuncs.com'),
+    'key'         => env('ALIYUN_STS_KEY', 'ALIYUN_STS_KEY'),
+    'secret'      => env('ALIYUN_STS_SECRET', 'ALIYUN_STS_SECRET'),
+    'role_arn'    => env('ALIYUN_STS_ROLE_ARN', 'ALIYUN_STS_ROLE_ARN'),
+    'expire_time' => env('ALIYUN_STS_EXPIRE_TIME', 900),
+    'policy'      => <<<POLICY
+        {
+          "Statement": [
+            {
+              "Action": [
+                "oss:Get*",
+                "oss:List*"
+              ],
+              "Effect": "Allow",
+              "Resource": "*"
+            }
+          ],
+          "Version": "1"
+        }
+POLICY
+
+];
---
+++ src/Sts.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liul
+ * Date: 10/23/18
+ * Time: 14:17
+ */
+
+namespace Mogull\Aliyun\Sts;
+
+use Sts\AssumeRoleRequest;
+use Sts\Core\DefaultAcsClient;
+use Sts\Core\Profile\DefaultProfile;
+
+class Sts
+{
+    protected $endpointName;
+    protected $regionId;
+    protected $domain;
+    protected $accessKeyId;
+    protected $accessSecret;
+    protected $roleArn;
+    protected $policy;
+    protected $clientName;
+    protected $duration;
+
+    /**
+     * Sts constructor.
+     * @param $endpointName
+     * @param $regionId
+     * @param $domain
+     * @param $accessKeyId
+     * @param $accessSecret
+     * @param $roleArn
+     * @param $policy
+     * @param string $clientName
+     * @param int $duration
+     */
+    public function __construct($endpointName, $regionId, $domain, $accessKeyId, $accessSecret, $roleArn, $policy, $clientName = '', $duration = 900)
+    {
+        $this->endpointName = $endpointName;
+        $this->regionId = $regionId;
+        $this->domain = $domain;
+        $this->accessKeyId = $accessKeyId;
+        $this->accessSecret = $accessSecret;
+        $this->roleArn = $roleArn;
+        $this->policy = $policy;
+        $this->clientName = $clientName;
+        $this->duration = $duration;
+    }
+
+    /**
+     * @return AssumeRoleRequest
+     */
+    public function createStsRequest()
+    {
+        $request = new AssumeRoleRequest();
+        $request->setRoleSessionName($this->clientName);
+        $request->setRoleArn($this->roleArn);
+        $request->setPolicy($this->policy);
+        $request->setDurationSeconds($this->duration);
+
+        return $request;
+    }
+
+    /**
+     * @return DefaultAcsClient
+     */
+    private function client()
+    {
+        DefaultProfile::addEndpoint($this->endpointName, $this->regionId, "Sts", $this->domain);
+        $iClientProfile = DefaultProfile::getProfile($this->regionId, $this->accessKeyId, $this->accessSecret);
+        $client = new DefaultAcsClient($iClientProfile);
+
+        return $client;
+    }
+
+    /**
+     * @param AssumeRoleRequest $request
+     * @return mixed|\SimpleXMLElement
+     */
+    public function getResult($request)
+    {
+        return $this->client()->getAcsResponse($request);
+    }
+
+    /**
+     * @param AssumeRoleRequest $request
+     * @return null|\SimpleXMLElement[]
+     */
+    public function getCredentials($request)
+    {
+        $result = $this->getResult($request);
+        return $result ? $result->Credentials : null;
+    }
+}
